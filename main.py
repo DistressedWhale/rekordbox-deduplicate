b94f65363fdaf2ea01500f95ed849b0561e81841
Chlowah <chloewhale21@protonmail.com>
2025-03-01 23:37:59 +0000

    Last attempts to make old method work

--- main.py
+++ main.py
@@ -6,6 +6,68 @@ import inspect, json, warnings, itertools
 from pyrekordbox import Rekordbox6Database
 from datetime import datetime
 
+# Replace all occurrences of a song in a playlist with another song
+def replace_songs(playlist_id, song_id1, song_id2):
+    db = Rekordbox6Database()
+
+    print(f"Processing playlist {db.get_playlist(ID=playlist_id).Name} (ID: {playlist_id})")
+
+    replacement_song = db.get_content(ID=song_id2)
+    
+    id_list = [int(getattr(x.Content, "ID", "-1")) for x in db.get_playlist(ID=playlist_id).Songs]
+
+    print(id_list)
+
+    while song_id1 in id_list:
+        replaced = False
+        print(f"Found {id_list.count(song_id1)} matching SongPlaylist objects")
+        playlist = db.get_playlist(ID=playlist_id)
+
+        songplaylists = playlist.Songs
+        for songplaylist in songplaylists:
+            
+            song = songplaylist.Content
+            song_id = song.ID
+
+            #Skip past non-matching songs
+            if replaced or str(song_id) != str(song_id1):
+                continue
+            else:
+                location = songplaylist.TrackNo
+
+                print(f"\tReplacing {song.Title}, position {location} with {replacement_song.Title}")
+
+                db.add_to_playlist(playlist, replacement_song, track_no=location)
+
+
+                
+
+                
+                #Update list of IDs for next loop
+                id_list = [int(getattr(x.Content, "ID", "-1")) for x in db.get_playlist(ID=playlist_id).Songs]
+        
+    db.close()
+    
+
+# def get_song_by_id(inp_id):
+#     db = Rekordbox6Database()
+
+#     dbcontent = db.get_content()
+
+#     for index, content in enumerate(dbcontent):
+#         if content.ID == str(inp_id):
+#             return content
+
+# def get_playlist_by_id(inp_id):
+#     db = Rekordbox6Database()
+
+#     dbcontent = db.get_playlist()
+
+#     for index, content in enumerate(dbcontent):
+#         if content.ID == str(inp_id):
+#             return content
+    
+
 def index_to_id(index, li):
     return li[index].ID
 
@@ -29,14 +91,14 @@ def all_equal(iterable):
     g = itertools.groupby(iterable)
     return next(g, True) and not next(g, False)
 
-def print_strings(obj, indent=0, visited=None, file=None, skip_recurse={}):
+def dump_object(obj, indent=0, visited=None, file=None, skip_recurse={}):
     """
     Recursively prints string attributes and values from an object,
     avoiding infinite recursion by tracking visited objects.
     Handles SQLAlchemy-related attribute access issues.
     Outputs the results to a file if provided.
     """
-    skip_recurse = {"imag", "MixerParams", "Cues", "created_at", "updated_at", "MyTagIDs", "MyTagNames", "MyTags", "Artist", "Genre", "Key", "Album", "Analysed"}  # List of parameters to output but not recurse on
+    # skip_recurse = {"imag", "MixerParams", "Cues", "created_at", "updated_at", "MyTagIDs", "MyTagNames", "MyTags", "Artist", "Genre", "Key", "Album", "Analysed"}  # List of parameters to output but not recurse on
     
     if visited is None:
         visited = set()
@@ -55,14 +117,14 @@ def print_strings(obj, indent=0, visited=None, file=None, skip_recurse={}):
         output(" " * indent + obj)
     elif isinstance(obj, (list, tuple, set)):
         for item in obj:
-            print_strings(item, indent + 2, visited, file)
+            dump_object(item, indent + 2, visited, file, skip_recurse=skip_recurse)
     elif isinstance(obj, dict):
         for key, value in obj.items():
             output(f"{' ' * indent}{key}: ")
             if key in skip_recurse:
                 output(f"{' ' * (indent + 2)}{value}")
             else:
-                print_strings(value, indent + 2, visited, file)
+                dump_object(value, indent + 2, visited, file, skip_recurse=skip_recurse)
     else:
         # Suppress SQLAlchemy deprecation warnings
         with warnings.catch_warnings():
@@ -80,7 +142,7 @@ def print_strings(obj, indent=0, visited=None, file=None, skip_recurse={}):
                         output(f"{' ' * indent}{name}: {value}")
                     else:
                         output(f"{' ' * indent}{name}:")
-                        print_strings(value, indent + 2, visited, file)
+                        dump_object(value, indent + 2, visited, file, skip_recurse=skip_recurse)
                 except Exception:
                     continue  # Skip attributes that raise exceptions when accessed
 
@@ -138,11 +200,7 @@ def grouped_non_unique_indexes(strings):
     return grouped_indexes
 
 
-# def dump_song_data():
-
-
-if __name__ == "__main__":
-
+def dump_song_data():
     db = Rekordbox6Database()
 
     dbcontent = db.get_content()
@@ -151,8 +209,8 @@ if __name__ == "__main__":
 
     for index, content in enumerate(dbcontent):
 
-        with open("output.txt", "w", encoding="utf-8") as f:
-            print_strings(content, file=f)
+        with open("song_dump.yaml", "w", encoding="utf-8") as f:
+            dump_object(content, file=f, skip_recurse = {"imag", "MixerParams", "Cues", "created_at", "updated_at", "MyTagIDs", "MyTagNames", "MyTags", "Artist", "Genre", "Key", "Album", "Analysed"})
 
             json_formatted = {
                 "ID": content.ID,
@@ -163,7 +221,6 @@ if __name__ == "__main__":
                 "AlbumName": content.AlbumName,
                 "ArtistName": content.ArtistName,
                 "ArtistID": content.ArtistID,
-                "DateCreated": content.DateCreated,
                 "FolderPath": content.FolderPath,
                 "BPM": content.BPM,
                 "BitRate": content.BitRate,
@@ -172,17 +229,39 @@ if __name__ == "__main__":
 
             content_list.append(json_formatted)
 
-    with open("output.json", "w") as f:
+    with open("song_data.json", "w") as f:
         json.dump(content_list, f, indent=4)
+    
+    return content_list
 
+def dump_playlist_data():
+    db = Rekordbox6Database()
 
-    transposed_list = transpose_dicts(content_list)
-    non_unique_indexes = grouped_non_unique_indexes(transposed_list["FullName"])
+    dbcontent = db.get_playlist()
 
-    # print(non_unique_indexes)
-    print(f"{len(non_unique_indexes)} duplicate tracks found")
+    content_list = []
 
+    for index, content in enumerate(dbcontent):
 
+        with open("playlist_data.yaml", "w", encoding="utf-8") as f:
+            dump_object(content, file=f, skip_recurse = {"imag", "MixerParams", "Cues", "created_at", "updated_at", "MyTagIDs", "MyTagNames", "MyTags", "Artist", "Genre", "Key", "Album", "Analysed", "Parent"})
+
+            json_formatted = {
+                "ID": content["ID"],
+                "index": index,
+                "Name": content.Name,
+                "Attribute": content.Attribute,
+                "SongIDs": [getattr(x.Content, "ID", "No ID") for x in content.Songs]
+            }
+
+            content_list.append(json_formatted)
+
+    with open("playlist_data.json", "w") as f:
+        json.dump(content_list, f, indent=4)
+    
+    return content_list
+
+def deduplicate(content_list, non_unique_indexes):
     #Come up with a final list of indexes/ID's to remove, based on criteria
     #1. Higher bitrate
     #2. FolderPath contains "imported from device" (prioritise your own files)
@@ -261,10 +340,38 @@ if __name__ == "__main__":
         stats["first_index"] += 1
  
     print(stats)
-    print(best_indexes)
+    # print(best_indexes)
 
     best_songs = [{str(non_unique_indexes[i][index]) : non_unique_indexes[i]} for i, index in enumerate(best_indexes)]
 
-    print(best_songs)
+    return best_songs
+
+if __name__ == "__main__":
+    #Get song data in a useful format
+    content_list = dump_song_data()
+
+    #Transpose list to assist with searching/filtering by attributes
+    transposed_list = transpose_dicts(content_list)
+
+    #Find the indexes of non unique items (List of lists) based on the full name (Song title and artist name combined)
+    non_unique_indexes = grouped_non_unique_indexes(transposed_list["FullName"])
+
+    print(f"{len(non_unique_indexes)} duplicate tracks found")
+
+    #Apply filter criteria to each group to identify the best member of each group.
+    #Return a list of dicts with the best item as the index 
+    # [ {'46': [46, 1528]}, {'68': [68, 1503]}, {'97': [97, 1567, 1606]}, {'130': [130, 1508]}, {'135': [135, 1247]}, {'137': [137, 671]} ]
+    best_songs = deduplicate(content_list, non_unique_indexes)
+    # print(best_songs)
+
+    # input("\nWould you like to proceed with deduplication? Use Ctrl+C to exit if not\n>>> ")
+
+    #Retrieve playlists 
+    dump_playlist_data()   
+    
 
     #Apply the changes to the database - replacing songs in playlists as needed
+
+
+
+    
--- xmlstuff.py
+++
@@ -1,7 +0,0 @@
-from pyrekordbox.rbxml import RekordboxXml
-
-xml = RekordboxXml("database.xml")
-
-track = xml.get_track(253337680)
-
-print(track)
